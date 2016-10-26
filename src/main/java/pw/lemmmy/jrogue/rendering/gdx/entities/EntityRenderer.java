a67039ba9838e53f957b76cc643e2c1673f40df6
Drew Lemmy <drew@lemmmy.pw>
2016-10-26 23:24:35 +0100

    Removed drawLight from entity renderer

--- src/main/java/pw/lemmmy/jrogue/rendering/gdx/entities/EntityRenderer.java
+++ src/main/java/pw/lemmmy/jrogue/rendering/gdx/entities/EntityRenderer.java
@@ -27,32 +27,4 @@ public abstract class EntityRenderer {
 			batch.draw(image, x * width + 0.01f, y * height + 0.01f);
 		}
 	}
-
-	public void drawLight(ShapeRenderer batch, Dungeon dungeon, int x, int y) {
-		int width = TileMap.TILE_WIDTH;
-		int height = TileMap.TILE_HEIGHT;
-
-		Color ctl = Color.BLACK;
-		Color ctr = Color.BLACK;
-		Color cbr = Color.BLACK;
-		Color cbl = Color.BLACK;
-
-		Tile tl = dungeon.getLevel().getTileInfo(x, y);
-		Tile tr = dungeon.getLevel().getTileInfo(x + 1, y);
-		Tile br = dungeon.getLevel().getTileInfo(x + 1, y + 1);
-		Tile bl = dungeon.getLevel().getTileInfo(x, y + 1);
-
-		if (tl != null) ctl = tl.getLight();
-		if (tr != null) ctr = tr.getLight();
-		if (br != null) cbr = br.getLight();
-		if (bl != null) cbl = bl.getLight();
-
-		batch.rect(
-			x * width, y * height, width, height,
-			Utils.awtColourToGdx(ctl),
-			Utils.awtColourToGdx(ctr),
-			Utils.awtColourToGdx(cbr),
-			Utils.awtColourToGdx(cbl)
-		);
-	}
 }
