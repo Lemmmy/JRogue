2f5f5656ac66475158cff42f3c3932b93146435a
Drew Lemmy <drew@lemmmy.pw>
2016-11-27 17:52:38 +0000

    Set the zoom back to what it was before

--- src/main/java/pw/lemmmy/jrogue/rendering/gdx/GDXRenderer.java
+++ src/main/java/pw/lemmmy/jrogue/rendering/gdx/GDXRenderer.java
@@ -134,7 +134,7 @@ public class GDXRenderer extends ApplicationAdapter implements Renderer, Dungeon
 
 		updateWindowTitle();
 
-		zoom = 72 * TileMap.TILE_WIDTH;
+		zoom = 24 * TileMap.TILE_WIDTH;
 
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
