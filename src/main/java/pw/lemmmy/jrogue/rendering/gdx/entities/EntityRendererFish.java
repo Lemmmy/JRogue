b617780e9499cd3d7a2bd35d1159f86e7849da09
Drew Lemmy <drew@lemmmy.pw>
2016-12-02 20:29:58 +0000

    re-added transparent fish

--- src/main/java/pw/lemmmy/jrogue/rendering/gdx/entities/EntityRendererFish.java
+++ src/main/java/pw/lemmmy/jrogue/rendering/gdx/entities/EntityRendererFish.java
@@ -16,7 +16,7 @@ public class EntityRendererFish extends EntityRenderer {
 	@Override
 	public void draw(SpriteBatch batch, Dungeon dungeon, Entity entity) {
 		Color c = batch.getColor();
-		// batch.setColor(c.r, c.g, c.b, 0.7f);
+		batch.setColor(c.r, c.g, c.b, 0.7f);
 		drawTile(batch, image, entity.getX(), entity.getY());
 		batch.setColor(c);
 	}
