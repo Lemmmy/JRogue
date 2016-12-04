bfeb8aef8c9a6a60d805aa27c6f818b3e9b27d3c
Drew Lemmy <drew@lemmmy.pw>
2016-12-04 00:13:17 +0000

    bugfix

--- src/main/java/pw/lemmmy/jrogue/rendering/gdx/GDXRenderer.java
+++ src/main/java/pw/lemmmy/jrogue/rendering/gdx/GDXRenderer.java
@@ -334,7 +334,7 @@ public class GDXRenderer extends ApplicationAdapter implements Renderer, Dungeon
 		for (Iterator<EntityPooledEffect> iterator = entityPooledEffects.iterator(); iterator.hasNext(); ) {
 			EntityPooledEffect effect = iterator.next();
 
-			if (!dungeon.getLevel().isTileDiscovered(effect.getX(), effect.getY()) ||
+			if (!dungeon.getLevel().isTileDiscovered(effect.getEntity().getX(), effect.getEntity().getY()) ||
 				!effect.getRenderer().shouldDrawParticles(dungeon, effect.getEntity(), effect.getEntity().getX(), effect.getEntity().getY())) {
 				continue;
 			}
