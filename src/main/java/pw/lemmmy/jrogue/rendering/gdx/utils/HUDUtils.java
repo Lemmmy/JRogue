795c6233d46ab0d6c04f14ebbea6b99f3eee601c
DDDDDDDDDdfsasdsafsd <Lemmmy@users.noreply.github.com>
2016-12-05 15:35:52 +0000

    Create HUDUtils.java

---
+++ src/main/java/pw/lemmmy/jrogue/rendering/gdx/utils/HUDUtils.java
@@ -0,0 +1,24 @@
+package pw.lemmmy.jrogue.rendering.gdx.utils;
+
+public class HUDUtils {
+    public String getHealthColour(int health, int maxHealth) {
+        if (health <= maxHealth / 5) {
+            return "P_RED";
+        } else if (health <= maxHealth / 3) {
+            return "P_ORANGE_3";
+        } else if (health <= maxHealth / 2) {
+            return "P_YELLOW";
+        } else {
+            return "P_GREEN_3";
+        }
+    }
+    
+	public String replaceMarkupString(String s) {
+		s = s.replace("[GREEN]", "[P_GREEN_3]");
+		s = s.replace("[CYAN]", "[P_CYAN_1]");
+		s = s.replace("[BLUE]", "[P_BLUE_1]");
+		s = s.replace("[YELLOW]", "[P_YELLOW]");
+
+		return s;
+	}
+}
