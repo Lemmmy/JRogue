55ce12e02ee27d10c2fa238414d147ebd80e7f6e
DDDDDDDDDdfsasdsafsd <Lemmmy@users.noreply.github.com>
2016-12-05 15:36:39 +0000

    reindent

--- src/main/java/pw/lemmmy/jrogue/rendering/gdx/utils/HUDUtils.java
+++ src/main/java/pw/lemmmy/jrogue/rendering/gdx/utils/HUDUtils.java
@@ -1,17 +1,17 @@
 package pw.lemmmy.jrogue.rendering.gdx.utils;
 
 public class HUDUtils {
-    public String getHealthColour(int health, int maxHealth) {
-        if (health <= maxHealth / 5) {
-            return "P_RED";
-        } else if (health <= maxHealth / 3) {
-            return "P_ORANGE_3";
-        } else if (health <= maxHealth / 2) {
-            return "P_YELLOW";
-        } else {
-            return "P_GREEN_3";
-        }
-    }
+	public String getHealthColour(int health, int maxHealth) {
+		if (health <= maxHealth / 5) {
+			return "P_RED";
+		} else if (health <= maxHealth / 3) {
+			return "P_ORANGE_3";
+		} else if (health <= maxHealth / 2) {
+			return "P_YELLOW";
+		} else {
+			return "P_GREEN_3";
+		}
+	}
     
 	public String replaceMarkupString(String s) {
 		s = s.replace("[GREEN]", "[P_GREEN_3]");
