833ddc5e3e9f4e2b05c5149d32bf6ff92433b69e
Lignum <grgmckinlay@gmail.com>
2016-12-23 18:05:27 +0100

    Correctly suppress warnings

--- src/main/java/pw/lemmmy/jrogue/utils/OpenSimplexNoise.java
+++ src/main/java/pw/lemmmy/jrogue/utils/OpenSimplexNoise.java
@@ -16,7 +16,7 @@ package pw.lemmmy.jrogue.utils;
  *   will be the same when ported to other languages.
  */
 
-@SuppressWarnings("ALL")
+@SuppressWarnings("all")
 public class OpenSimplexNoise {
 	private static final double STRETCH_CONSTANT_2D = -0.211324865405187;    //(1/Math.sqrt(2+1)-1)/2;
 	private static final double SQUISH_CONSTANT_2D = 0.366025403784439;      //(Math.sqrt(2+1)-1)/2;
