3edaa42199a1282d24abcceb91ae689b8472c41a
Drew Lemmy <drew@lemmmy.pw>
2016-12-27 13:38:29 +0000

    Abstract the room generator for future re-use

---
+++ src/main/java/pw/lemmmy/jrogue/dungeon/generators/RoomGenerator.java
@@ -0,0 +1,352 @@
+package pw.lemmmy.jrogue.dungeon.generators;
+
+import pw.lemmmy.jrogue.JRogue;
+import pw.lemmmy.jrogue.dungeon.Level;
+import pw.lemmmy.jrogue.dungeon.entities.QuickSpawn;
+import pw.lemmmy.jrogue.dungeon.generators.rooms.Room;
+import pw.lemmmy.jrogue.dungeon.generators.rooms.RoomBasic;
+import pw.lemmmy.jrogue.dungeon.generators.rooms.RoomFountain;
+import pw.lemmmy.jrogue.dungeon.generators.rooms.RoomWater;
+import pw.lemmmy.jrogue.dungeon.tiles.Tile;
+import pw.lemmmy.jrogue.dungeon.tiles.TileStateClimbable;
+import pw.lemmmy.jrogue.dungeon.tiles.TileType;
+import pw.lemmmy.jrogue.utils.Path;
+import pw.lemmmy.jrogue.utils.Utils;
+import pw.lemmmy.jrogue.utils.WeightedCollection;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public abstract class RoomGenerator extends DungeonGenerator {
+	private static final WeightedCollection<Class<? extends Room>> ROOM_TYPES = new WeightedCollection<>();
+	
+	static {
+		ROOM_TYPES.add(40, RoomBasic.class);
+		ROOM_TYPES.add(4, RoomFountain.class);
+		ROOM_TYPES.add(1, RoomWater.class);
+	}
+	
+	private static final int MIN_ROOM_WIDTH = 5;
+	private static final int MAX_ROOM_WIDTH = 20;
+	
+	private static final int MIN_ROOM_HEIGHT = 5;
+	private static final int MAX_ROOM_HEIGHT = 9;
+	
+	private static final int MIN_ROOM_DISTANCE_X = 1;
+	private static final int MAX_ROOM_DISTANCE_X = 15;
+	private static final int MIN_ROOM_OFFSET_X = -4;
+	private static final int MAX_ROOM_OFFSET_X = 4;
+	
+	private static final int MIN_ROOM_DISTANCE_Y = 1;
+	private static final int MAX_ROOM_DISTANCE_Y = 5;
+	private static final int MIN_ROOM_OFFSET_Y = -4;
+	private static final int MAX_ROOM_OFFSET_Y = 4;
+	
+	private static final float CORRIDOR_LINE_SLOPE = 0.2f;
+	
+	private static final double PROBABILITY_GOLD_DROP = 0.08;
+	
+	private VerificationPathfinder pathfinder = new VerificationPathfinder();
+	
+	private Tile startTile;
+	private Tile endTile;
+	
+	public RoomGenerator(Level level, Optional<Tile> sourceTile) {
+		super(level, sourceTile);
+	}
+	
+	@Override
+	public boolean generate() {
+		int width = nextInt(MIN_ROOM_WIDTH, MAX_ROOM_WIDTH);
+		int height = nextInt(MIN_ROOM_HEIGHT, MAX_ROOM_HEIGHT);
+				
+		createRoom(
+			nextInt(1, level.getWidth() - width - 1),
+			nextInt(1, level.getHeight() - height - 1),
+			width,
+			height
+		);
+		
+		Collections.shuffle(rooms);
+		
+		graphRooms();
+		buildCorridors();
+		removeStrayRooms();
+		if (!chooseSpawnRoom()) { return false; }
+		chooseNextStairsRoom();
+		addRoomFeatures();
+		addRandomDrops();
+		
+		return true;
+	}
+	
+	
+	private void createRoom(int roomX, int roomY, int roomWidth, int roomHeight) {
+		buildRoom(ROOM_TYPES.next(), roomX, roomY, roomWidth, roomHeight);
+		
+		for (int[] direction : Utils.DIRECTIONS) {
+			int attempts = 0;
+			
+			while (attempts < 5) {
+				attempts++;
+				
+				int newRoomWidth = nextInt(MIN_ROOM_WIDTH, MAX_ROOM_WIDTH);
+				int newRoomHeight = nextInt(MIN_ROOM_HEIGHT, MAX_ROOM_HEIGHT);
+				
+				int newRoomX = roomX + (direction[0] * roomWidth) +
+					(direction[0] * nextInt(MIN_ROOM_DISTANCE_X, MAX_ROOM_DISTANCE_X)) +
+					(direction[1] * nextInt(MIN_ROOM_OFFSET_X, MAX_ROOM_OFFSET_X));
+				int newRoomY = roomY + (direction[1] * roomHeight) +
+					(direction[1] * nextInt(MIN_ROOM_DISTANCE_Y, MAX_ROOM_DISTANCE_Y)) +
+					(direction[0] * nextInt(MIN_ROOM_OFFSET_Y, MAX_ROOM_OFFSET_Y));
+				
+				if (canBuildRoom(newRoomX, newRoomY, newRoomWidth, newRoomHeight)) {
+					createRoom(newRoomX, newRoomY, newRoomWidth, newRoomHeight);
+					
+					break;
+				}
+			}
+		}
+	}
+	
+	private void graphRooms() {
+		for (Room a : rooms) {
+			for (Room b : rooms) {
+				boolean skip = false;
+				
+				double abDist = Math.pow(a.getCenterX() - b.getCenterX(), 2) + Math
+					.pow(a.getCenterY() - b.getCenterY(), 2);
+				
+				for (Room c : rooms) {
+					if (c.equals(a) || c.equals(b)) {
+						continue;
+					}
+					
+					double acDist = Math.pow(a.getCenterX() - c.getCenterX(), 2) + Math
+						.pow(a.getCenterY() - c.getCenterY(), 2);
+					double bcDist = Math.pow(b.getCenterX() - c.getCenterX(), 2) + Math
+						.pow(b.getCenterY() - c.getCenterY(), 2);
+					
+					if (acDist < abDist && bcDist < abDist) {
+						skip = true;
+					}
+					
+					if (skip) {
+						break;
+					}
+				}
+				
+				if (!skip) {
+					a.addTouching(b);
+				}
+			}
+		}
+	}
+	
+	private void buildCorridors() {
+		for (Room a : rooms) {
+			for (Room b : a.getTouching()) {
+				float dx = b.getCenterX() - a.getCenterX();
+				float dy = b.getCenterY() - a.getCenterY();
+				
+				if (dx > 0) {
+					float slope = Math.abs(dy / dx);
+					
+					if (slope > 0.5f) {
+						slope = Math.abs(-1f / slope);
+					}
+					
+					ConnectionPoint point = getConnectionPoint(a, b);
+					
+					a.addConnectionPoint(point);
+					b.addConnectionPoint(point);
+					
+					if (slope <= CORRIDOR_LINE_SLOPE) {
+						TileType tile = TileType.TILE_CORRIDOR;
+						
+						buildLine(point.getAX(), point.getAY(), point.getBX(), point.getBY(), tile, true, false);
+					} else {
+						if (point.getOrientationA() == point.getOrientationB()) {
+							buildSCorridor(point);
+						} else {
+							buildLCorridor(point);
+						}
+					}
+					
+					safePlaceDoor(point.getAX(), point.getAY());
+					safePlaceDoor(point.getBX(), point.getBY());
+				}
+			}
+		}
+	}
+	
+	private void buildLCorridor(ConnectionPoint point) {
+		int ax = point.getAX();
+		int ay = point.getAY();
+		
+		int bx = point.getBX();
+		int by = point.getBY();
+		
+		int dx = bx - ax;
+		int dy = by - ay;
+		
+		TileType tile = TileType.TILE_CORRIDOR;
+		
+		if (Math.abs(dx) < 1 || Math.abs(dy) < 1) {
+			buildLine(ax, ay, bx, by, tile, true, true);
+			
+			return;
+		}
+		
+		buildLine(ax, ay, bx, ay, tile, true, true);
+		buildLine(bx, ay, bx, by, tile, true, true);
+	}
+	
+	private void buildSCorridor(ConnectionPoint point) {
+		int ax = point.getAX();
+		int ay = point.getAY();
+		
+		int bx = point.getBX();
+		int by = point.getBY();
+		
+		int dx = bx - ax;
+		int dy = by - ay;
+		
+		TileType tile = TileType.TILE_CORRIDOR;
+		
+		if (point.getIntendedOrientation() == Orientation.HORIZONTAL) {
+			buildLine(ax, ay, ax + (int) Math.ceil(dx / 2), ay, tile, true, true);
+			buildLine(ax + Math.round(dx / 2), ay, ax + (int) Math.floor(dx / 2), by, tile, true, true);
+			buildLine(bx, by, ax + (int) Math.floor(dx / 2), by, tile, true, true);
+		} else {
+			buildLine(ax, ay, ax, ay + (int) Math.ceil(dy / 2), tile, true, true);
+			buildLine(ax, ay + Math.round(dy / 2), bx, ay + (int) Math.floor(dy / 2), tile, true, true);
+			buildLine(bx, by, bx, ay + (int) Math.floor(dy / 2), tile, true, true);
+		}
+	}
+	
+	private void removeStrayRooms() {
+		rooms.removeIf(room -> room.getConnectionPoints().size() <= 0);
+	}
+	
+	private void addRoomFeatures() {
+		rooms.forEach(Room::addFeatures);
+	}
+	
+	private void addRandomDrops() {
+		rooms.forEach(r -> {
+			if (rand.nextDouble() < PROBABILITY_GOLD_DROP) {
+				int x = rand.nextInt(r.getRoomWidth() - 2) + r.getRoomX() + 1;
+				int y = rand.nextInt(r.getRoomHeight() - 2) + r.getRoomY() + 1;
+				
+				QuickSpawn.spawnGold(level, x, y, Utils.roll(Math.abs(level.getDepth()) + 2, 6));
+			}
+		});
+	}
+	
+	private boolean chooseSpawnRoom() {
+		List<Room> temp = new ArrayList<>(rooms);
+		temp.sort(Comparator.comparingInt(a -> a.getConnectionPoints().size()));
+		
+		List<Room> temp2 = temp.stream()
+			.filter(room -> room.getConnectionPoints().size() == temp.get(temp.size() - 1)
+				.getConnectionPoints().size())
+			.collect(Collectors.toList());
+		
+		if (temp2.isEmpty()) {
+			return false;
+		}
+		
+		Room spawnRoom = Utils.randomFrom(temp2);
+		
+		int stairX = nextInt(spawnRoom.getRoomX() + 2, spawnRoom.getRoomX() + spawnRoom.getRoomWidth() - 2);
+		int stairY = nextInt(spawnRoom.getRoomY() + 2, spawnRoom.getRoomY() + spawnRoom.getRoomHeight() - 2);
+		
+		if (sourceTile.isPresent()) {
+			Tile spawnTile = level.getTile(stairX, stairY);
+			spawnTile.setType(TileType.TILE_ROOM_STAIRS_UP);
+			
+			Tile sourceTile = this.sourceTile.get();
+			
+			if (sourceTile.getLevel().getDepth() < level.getDepth()) {
+				spawnTile.setType(TileType.TILE_ROOM_STAIRS_DOWN);
+			}
+			
+			if (spawnTile.getState() instanceof TileStateClimbable) {
+				TileStateClimbable tsc = (TileStateClimbable) spawnTile.getState();
+				tsc.setLinkedLevelUUID(sourceTile.getLevel().getUUID());
+				tsc.setDestPosition(sourceTile.getX(), sourceTile.getY());
+			}
+		}
+		
+		spawnRoom.setSpawn();
+		startTile = level.getTile(stairX, stairY);
+		level.setSpawnPoint(stairX, stairY);
+		
+		return true;
+	}
+	
+	private void chooseNextStairsRoom() {
+		Optional<Room> spawnRoom = rooms.stream()
+			.filter(Room::isSpawn)
+			.findFirst();
+		
+		List<Room> temp = rooms.stream()
+			.filter(room -> !room.isSpawn())
+			.collect(Collectors.toList());
+		
+		spawnRoom.ifPresent(room -> temp.sort(Comparator.comparingDouble(a -> Utils.distanceSq(
+			a.getCenterX(),
+			a.getCenterY(),
+			spawnRoom.get().getCenterX(),
+			spawnRoom.get().getCenterX()
+			))
+		));
+		
+		List<Room> possibleRooms = temp.stream()
+			.skip(temp.size() - 5)
+			.collect(Collectors.toList());
+		
+		Room nextStairsRoom = Utils.randomFrom(possibleRooms);
+		
+		int stairX = nextInt(
+			nextStairsRoom.getRoomX() + 2,
+			nextStairsRoom.getRoomX() + nextStairsRoom.getRoomWidth() - 2
+		);
+		int stairY = nextInt(
+			nextStairsRoom.getRoomY() + 2,
+			nextStairsRoom.getRoomY() + nextStairsRoom.getRoomHeight() - 2
+		);
+		
+		level.setTileType(stairX, stairY, TileType.TILE_ROOM_STAIRS_DOWN);
+		
+		if (sourceTile.isPresent()) {
+			Tile st = sourceTile.get();
+			
+			if (st.getLevel().getDepth() < level.getDepth()) {
+				level.setTileType(stairX, stairY, TileType.TILE_ROOM_STAIRS_UP);
+			}
+		}
+		
+		endTile = level.getTile(stairX, stairY);
+	}
+	
+	protected boolean verify() {
+		Path path = pathfinder.findPath(
+			level,
+			startTile.getX(),
+			startTile.getY(),
+			endTile.getX(),
+			endTile.getY(),
+			Integer.MAX_VALUE,
+			true,
+			new ArrayList<>()
+		);
+		
+		if (path == null) {
+			JRogue.getLogger().debug("Level was generated unreachable - regenerating");
+		}
+		
+		return path != null;
+	}
+}
