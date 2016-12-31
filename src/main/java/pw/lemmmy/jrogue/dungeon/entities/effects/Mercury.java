91f0cc857609b49e22cda3429a8d064e94be67ea
Drew Lemmy <drew@lemmmy.pw>
2016-12-31 16:08:19 +0000

    nerfed mercury

--- src/main/java/pw/lemmmy/jrogue/dungeon/entities/effects/Mercury.java
+++ src/main/java/pw/lemmmy/jrogue/dungeon/entities/effects/Mercury.java
@@ -1,16 +1,46 @@
 package pw.lemmmy.jrogue.dungeon.entities.effects;
 
 import pw.lemmmy.jrogue.dungeon.entities.DamageSource;
+import pw.lemmmy.jrogue.dungeon.entities.LivingEntity;
 
-public class Mercury extends Poison {
+public class Mercury extends StatusEffect {
+	public Mercury() {
+		this(-1);
+	}
+	
+	public Mercury(int duration) {
+		super(duration);
+	}
+	
+	@Override
+	public void turn() {
+		super.turn();
+		
+		if (getEntity() instanceof LivingEntity) {
+			LivingEntity livingEntity = (LivingEntity) getEntity();
+			
+			if (
+				livingEntity.getHealth() > 2 &&
+				livingEntity.getHealth() < livingEntity.getMaxHealth() - 2 &&
+				livingEntity.getDungeon().getTurn() % 5 == 0
+			) {
+				livingEntity.damage(getDamageSource(), 1, null, false);
+			}
+		}
+	}
+	
 	@Override
 	public String getName() {
 		return "Mercury";
 	}
 	
-	@Override
 	public DamageSource getDamageSource() {
 		return DamageSource.MERCURY;
+	}
+	
+	@Override
+	public Severity getSeverity() {
+		return Severity.CRITICAL;
 	}
 	
 	@Override
