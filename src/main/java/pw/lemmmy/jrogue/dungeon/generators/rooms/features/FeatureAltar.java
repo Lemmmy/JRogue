0e06c23b96947d8b84f7dedbe5457aa5be19b1fe
Drew Lemmy <drew@lemmmy.pw>
2017-01-18 21:29:47 +0000

    altar candlesticks also

--- src/main/java/pw/lemmmy/jrogue/dungeon/generators/rooms/features/FeatureAltar.java
+++ src/main/java/pw/lemmmy/jrogue/dungeon/generators/rooms/features/FeatureAltar.java
@@ -1,11 +1,15 @@
 package pw.lemmmy.jrogue.dungeon.generators.rooms.features;
 
 import com.github.alexeyr.pcg.Pcg32;
+import pw.lemmmy.jrogue.dungeon.entities.Entity;
+import pw.lemmmy.jrogue.dungeon.entities.decoration.EntityCandlestick;
 import pw.lemmmy.jrogue.dungeon.entities.magic.EntityAltar;
 import pw.lemmmy.jrogue.dungeon.generators.rooms.Room;
 import pw.lemmmy.jrogue.dungeon.tiles.TileType;
 
 public class FeatureAltar extends SpecialRoomFeature {
+	private static final double PROBABILITY_ALTAR_CANDLESTICK = 0.5;
+	
 	private Pcg32 rand = new Pcg32();
 	
 	@Override
@@ -22,6 +26,14 @@ public class FeatureAltar extends SpecialRoomFeature {
 					room.getLevel().setTileType(x, y, TileType.TILE_ROOM_RUG);
 				}
 			}
+		}
+		
+		if (rand.nextDouble() < PROBABILITY_ALTAR_CANDLESTICK) {
+			EntityCandlestick cs1 = new EntityCandlestick(room.getLevel().getDungeon(), room.getLevel(), altarX - 1, altarY);
+			room.getLevel().addEntity(cs1);
+			
+			EntityCandlestick cs2 = new EntityCandlestick(room.getLevel().getDungeon(), room.getLevel(), altarX + 1, altarY);
+			room.getLevel().addEntity(cs2);
 		}
 	}
 }
