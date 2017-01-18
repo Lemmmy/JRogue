1a43cd6def3560c1a6fb37f6f1546bece8e3ee52
Drew Lemmy <drew@lemmmy.pw>
2017-01-18 21:02:17 +0000

    oops!

--- classes/production/jrogue/shaders/global_repeat.frag.glsl
+++
@@ -1,9 +0,0 @@
-uniform sampler2D u_texture;
-
-varying vec2 v_texCoords;
-varying vec4 v_colour;
-varying vec4 v_worldPos;
-
-void main() {
-    gl_FragColor = v_colour * texture2D(u_texture, v_worldPos.xy);
-}
