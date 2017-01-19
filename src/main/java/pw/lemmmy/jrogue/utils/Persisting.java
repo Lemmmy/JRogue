9d1f52c625818a1edeeb5b36cce5ce8745fa8aaf
Lignum <grgmckinlay@gmail.com>
2017-01-19 19:10:17 +0100

    Add code to Persisting interface

--- src/main/java/pw/lemmmy/jrogue/utils/Persisting.java
+++ src/main/java/pw/lemmmy/jrogue/utils/Persisting.java
@@ -1,7 +1,20 @@
 package pw.lemmmy.jrogue.utils;
 
-/**
- * Created by Lignum on 19/01/2017.
- */
-public interface Persisting {
+import org.json.JSONObject;
+import pw.lemmmy.jrogue.dungeon.Serialisable;
+
+public interface Persisting extends Serialisable {
+    JSONObject getPersistence();
+
+    @Override
+    default void serialise(JSONObject obj) {
+        obj.put("persistence", getPersistence());
+    }
+
+    @Override
+    default void unserialise(JSONObject obj) {
+        for (String key : obj.getJSONObject("persistence").keySet()) {
+            getPersistence().put(key, obj.get(key));
+        }
+    }
 }
