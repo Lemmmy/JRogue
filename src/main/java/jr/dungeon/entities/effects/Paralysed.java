ad44fef44b3cb5102864e28fc19f4e2212384558
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-01-28 22:07:30 -0200

    Add a paralysis effect
    Currently, however, paralysis prevents the player from moving. This means
    that the dungeon will skip turns until the paralysis effect is cleared
    (this led to a lot of very fun debugging over Slack).
    
    @Lemmmmy @Lignumm if one of you could tackle the turn-skipping, that'd
    be great.

---
+++ src/main/java/jr/dungeon/entities/effects/Paralysed.java
@@ -0,0 +1,24 @@
+package jr.dungeon.entities.effects;
+
+import jr.utils.RandomUtils;
+
+public class Paralysed extends StatusEffect {
+
+    public Paralysed() {
+        super(RandomUtils.random(10, 20));
+        System.out.println("PARALYSED CONSTRUCTOR CALLED");
+    }
+
+    @Override
+    public String getName() { return "Paralysis"; }
+
+    @Override
+    public Severity getSeverity() {
+        return Severity.CRITICAL;
+    }
+
+    @Override
+    public void onEnd() {
+        getMessenger().greenYou("manage to break free of the paralysis"); // TODO betterer message
+    }
+}
