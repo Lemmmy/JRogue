8391a22d6e4071720d25d8fd1955ac3bf007cc2b
Drew Lemmy <drew@lemmmy.pw>
2017-02-27 21:39:43 +0000

    moved it to its own class

--- src/main/java/jr/rendering/gdx/components/hud/HUDComponent.java
+++ src/main/java/jr/rendering/gdx/components/hud/HUDComponent.java
@@ -3,7 +3,6 @@ package jr.rendering.gdx.components.hud;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import jr.Settings;
@@ -11,8 +10,6 @@ import jr.dungeon.Dungeon;
 import jr.dungeon.Prompt;
 import jr.dungeon.entities.Entity;
 import jr.dungeon.entities.events.EntityAttackedToHitRollEvent;
-import jr.dungeon.entities.events.EntityEnergyChangedEvent;
-import jr.dungeon.entities.events.EntityHealthChangedEvent;
 import jr.dungeon.entities.monsters.Monster;
 import jr.dungeon.entities.player.Attribute;
 import jr.dungeon.entities.player.Player;
@@ -24,7 +21,6 @@ import jr.rendering.gdx.components.hud.windows.*;
 import jr.rendering.gdx.tiles.TileMap;
 import jr.rendering.gdx.utils.HUDUtils;
 import jr.rendering.gdx.utils.ImageLoader;
-import jr.utils.Point;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
@@ -36,8 +32,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class HUDComponent extends RendererComponent {
-	private static final float TEXT_POPUP_DURATION = 0.4f;
-	
 	@Getter private Skin skin;
 	@Getter private Stage stage;
 	private Label playerLabel;
@@ -165,6 +159,8 @@ public class HUDComponent extends RendererComponent {
 		
 		root.top();
 		stage.addActor(root);
+		
+		dungeon.addListener(new TextPopups(this));
 	}
 	
 	@Override
@@ -352,64 +348,6 @@ public class HUDComponent extends RendererComponent {
 		} else {
 			effectsLabel.setText("");
 		}
-	}
-	
-	@DungeonEventHandler
-	public void onEntityHealthChanged(EntityHealthChangedEvent e) {
-		Entity entity = e.getEntity();
-		int delta = e.getNewHealth() - e.getOldHealth();
-		boolean positive = delta >= 0;
-		
-		showTextPopup(entity.getPosition(), String.format(
-			"[%s]%s%,d HP[]",
-			entity instanceof Player ? positive ? "P_GREEN_2" : "P_ORANGE_2" : positive ? "P_GREEN_1" : "P_RED",
-			positive ? "+" : "-",
-			Math.abs(delta)
-		));
-	}
-	
-	@DungeonEventHandler(invocationTime = DungeonEventInvocationTime.NEXT_TURN)
-	public void onPlayerEnergyChanged(EntityEnergyChangedEvent e) {
-		Entity entity = e.getEntity();
-		int delta = e.getNewEnergy() - e.getOldEnergy();
-		boolean positive = delta >= 0;
-		
-		showTextPopup(entity.getPosition(), String.format(
-			"[%s]%s%,d MP[]",
-			positive ? "P_PURPLE_3" : "P_PURPLE_1",
-			positive ? "+" : "-",
-			Math.abs(delta)
-		));
-	}
-	
-	private void showTextPopup(Point worldPos, String text) {
-		if (dungeon.getLevel().getVisibilityStore().isTileInvisible(worldPos)) {
-			return;
-		}
-		
-		int setting = settings.getTextPopup();
-		
-		if (setting == 0) {
-			return;
-		}
-		
-		Vector3 pos = renderer.getCamera().project(
-			new Vector3((worldPos.getX() + 0.5f) * TileMap.TILE_WIDTH, worldPos.getY() * TileMap.TILE_HEIGHT, 0)
-		);
-		
-		Table table = new Table(skin);
-		table.add(new Label(text, skin, setting == 1 ? "default" : "large"));
-		
-		stage.getRoot().addActor(table);
-		table.pack();
-		table.setPosition((int) pos.x - (int) (table.getWidth() / 2), (int) pos.y);
-		singleTurnActors.add(table);
-		
-		table.addAction(Actions.moveTo(table.getX(), table.getY() + TileMap.TILE_HEIGHT / 2, TEXT_POPUP_DURATION));
-		table.addAction(Actions.sequence(
-			Actions.delay(TEXT_POPUP_DURATION / 2),
-			Actions.fadeOut(TEXT_POPUP_DURATION))
-		);
 	}
 	
 	@DungeonEventHandler
