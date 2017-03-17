ccdfc6bee0059b8d8e4a6a53c76341479a934b4e
Drew Lemmy <drew@lemmmy.pw>
2017-03-17 12:56:27 +0000

    docced gradient and optimised it massively

--- src/main/java/jr/utils/Gradient.java
+++ src/main/java/jr/utils/Gradient.java
@@ -3,6 +3,10 @@ package jr.utils;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.Pool;
 
+/**
+ * Simple pooled two-point Gradient class. Takes a start and end colour, and lets you get the colour from any point in
+ * the gradient. Uses GDX colours.
+ */
 public class Gradient implements Pool.Poolable {
 	private static final Pool<Gradient> gradientPool = new Pool<Gradient>() {
 		@Override
@@ -10,36 +14,69 @@ public class Gradient implements Pool.Poolable {
 			return new Gradient();
 		}
 	};
-
+	
+	/**
+	 * The start colour of the gradient.
+	 */
 	private Color start;
+	/**
+	 * The last interpolated colour.
+	 */
+	private Color lastInterpolated;
+	/**
+	 * The end colour of the gradient.
+	 */
 	private Color end;
-
+	
+	/**
+	 * Do not instantiate manually! Use {@link Gradient#getGradient(Color, Color)} instead.
+	 */
 	private Gradient() {
 		reset();
 	}
-
+	
+	/**
+	 * Gets a new gradient from the pool.
+	 *
+	 * @param start The start colour of the gradient.
+	 * @param end The end colour of the gradient.
+	 *
+	 * @return The new Gradient instance.
+	 */
 	public static Gradient getGradient(Color start, Color end) {
 		Gradient g = gradientPool.obtain();
 		g.init(start, end);
 		return g;
 	}
-
+	
+	/**
+	 * Frees a gradient from the pool.
+	 *
+	 * @param gradient The gradient to free.
+	 */
 	public static void free(Gradient gradient) {
 		gradientPool.free(gradient);
 	}
 
 	private void init(Color start, Color end) {
 		this.start = start;
+		this.lastInterpolated = new Color();
 		this.end = end;
 	}
-
+	
+	/**
+	 * @param point The percentage of the gradient (from 0 to 1) to get the colour from.
+	 *
+	 * @return The interpolated colour.
+	 */
 	public Color getColourAtPoint(float point) {
 		float r = start.r * sigmoid(point) + end.r * (1 - sigmoid(point));
 		float g = start.g * sigmoid(point) + end.g * (1 - sigmoid(point));
 		float b = start.b * sigmoid(point) + end.b * (1 - sigmoid(point));
 		float a = start.a * sigmoid(point) + end.a * (1 - sigmoid(point));
 		
-		return new Color(r, g, b, a);
+		lastInterpolated.set(r, g, b, a);
+		return lastInterpolated;
 	}
 	
 	private static float sigmoid(float x) {
