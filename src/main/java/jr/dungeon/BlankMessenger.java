3e9f4b80b48ef8e8465d6ad1727bb7ec16c76373
Drew Lemmy <drew@lemmmy.pw>
2017-03-17 10:00:30 +0000

    n

--- src/main/java/jr/dungeon/BlankMessenger.java
+++ src/main/java/jr/dungeon/BlankMessenger.java
@@ -1,5 +1,8 @@
 package jr.dungeon;
 
+/**
+ * Blank messenger that does nothing when logging.
+ */
 public class BlankMessenger implements Messenger {
 	@Override
 	public void log(String s, Object... objects) {
