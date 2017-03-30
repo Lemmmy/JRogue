362ab6bf1b99d9ef97a1a6412fc1ba2377956011
Drew Lemmy <drew@lemmmy.pw>
2017-03-30 14:45:09 +0100

    made a start on death screens

---
+++ src/main/java/jr/rendering/screens/DeathScreen.java
@@ -0,0 +1,72 @@
+package jr.rendering.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import jr.JRogue;
+import jr.rendering.GameAdapter;
+import jr.rendering.components.hud.HUDSkin;
+
+public class DeathScreen extends ScreenAdapter {
+	private Skin skin;
+	private Stage stage;
+	
+	private GameAdapter game;
+	
+	public DeathScreen(GameAdapter game) {
+		this.game = game;
+		
+		skin = new HUDSkin();
+		ScreenViewport stageViewport = new ScreenViewport();
+		stageViewport.setUnitsPerPixel(1f / JRogue.getSettings().getHudScale());
+		stage = new Stage(stageViewport);
+		
+		initLayout(new Table(skin));
+		
+		Gdx.graphics.setTitle(GameAdapter.WINDOW_TITLE);
+	}
+	
+	private void initLayout(Table container) {
+		container.setFillParent(true);
+		container.row().fill().top();
+		
+		container.top().pad(32);
+		
+		stage.addActor(container);
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(delta);
+		stage.draw();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+		
+		stage.getViewport().update(width, height);
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		
+		stage.dispose();
+		skin.dispose();
+	}
+	
+	@Override
+	public void show() {
+		super.show();
+		
+		Gdx.input.setInputProcessor(stage);
+	}
+}
