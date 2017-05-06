cd36dea4f19fd8119515c92498a5198cad561577
Drew Lemmy <drew@lemmmy.pw>
2017-05-06 19:51:00 +0100

    grate things

--- src/main/java/jr/rendering/tiles/TileRendererWall.java
+++ src/main/java/jr/rendering/tiles/TileRendererWall.java
@@ -5,13 +5,20 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import jr.dungeon.Dungeon;
 import jr.dungeon.tiles.TileType;
 
+import java.util.Random;
+
 public class TileRendererWall extends TileRenderer {
+	private static final int PROBABILITY_GRATE = 6;
+	
 	private static TextureRegion wallH;
 	private static TextureRegion wallHPillar;
 	private static TextureRegion wallHPillarExtra;
 	private static TextureRegion wallV;
 	private static TextureRegion wallCT;
 	private static TextureRegion wallCB;
+	private static TextureRegion wallGrate;
+	
+	private Random rand = new Random();
 	
 	public TileRendererWall() {
 		if (wallH == null || wallV == null || wallCT == null || wallCB == null) {
@@ -21,6 +28,7 @@ public class TileRendererWall extends TileRenderer {
 			wallV = getImageFromSheet("textures/tiles.png", 0, 0);
 			wallCT = getImageFromSheet("textures/tiles.png", 2, 0);
 			wallCB = getImageFromSheet("textures/tiles.png", 3, 0);
+			wallGrate = getImageFromSheet("textures/tiles.png", 8, 2);
 		}
 	}
 	
@@ -59,7 +67,18 @@ public class TileRendererWall extends TileRenderer {
 	
 	@Override
 	public void draw(SpriteBatch batch, Dungeon dungeon, int x, int y) {
+		TileType[] adjacentTiles = dungeon.getLevel().tileStore.getAdjacentTileTypes(x, y);
+		
+		boolean h = adjacentTiles[0].isWallTile() || adjacentTiles[1].isWallTile();
+		boolean top = adjacentTiles[2].isInnerRoomTile();
+		
 		drawTile(batch, getTextureRegion(dungeon, x, y), x, y);
+		
+		rand.setSeed(x * y);
+		
+		if (h && top && rand.nextInt(100) <= PROBABILITY_GRATE) {
+			drawTile(batch, wallGrate, x, y);
+		}
 	}
 	
 	@Override
