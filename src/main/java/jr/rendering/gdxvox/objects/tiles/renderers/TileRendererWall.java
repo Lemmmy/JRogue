aeca8abc939543bbee48b173ba52953d62220144
Drew Lemmy <drew@lemmmy.pw>
2017-10-03 09:07:12 +0100

    not done

--- src/main/java/jr/rendering/gdxvox/objects/tiles/renderers/TileRendererWall.java
+++ src/main/java/jr/rendering/gdxvox/objects/tiles/renderers/TileRendererWall.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
 import jr.dungeon.tiles.Tile;
+import jr.dungeon.tiles.TileType;
 import jr.rendering.gdxvox.models.magicavoxel.ModelConverter;
 import jr.rendering.gdxvox.objects.tiles.TileRenderer;
 import jr.rendering.gdxvox.objects.tiles.TileRendererInstance;
@@ -23,9 +24,19 @@ public class TileRendererWall extends TileRenderer {
 	
 	@Override
 	public void tileAdded(Tile tile) {
+		int x = tile.getX();
+		int y = tile.getY();
+		TileType[] adjacentTiles = tile.getLevel().tileStore.getAdjacentTileTypes(x, y);
+		
+		boolean h = adjacentTiles[0].isWallTile() || adjacentTiles[1].isWallTile();
+		boolean v = adjacentTiles[2].isWallTile() || adjacentTiles[3].isWallTile();
+		
 		ModelInstance instance = new ModelInstance(model);
-		instance.transform.translate(tile.getX(), 0, tile.getY());
+		instance.transform.translate(x, 0, y);
 		instance.transform.scale(1f / 16f, 1f / 16f, 1f / 16f);
+		if (h) {
+			instance.transform.rotate(0, 1, 0, 90);
+		}
 		objectInstanceMap.put(tile, new TileRendererInstance(tile, instance));
 	}
 	
