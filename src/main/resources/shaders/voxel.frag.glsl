58d413ae0236645b598aaa6b166a39c6932694b8
Drew Lemmy <drew@lemmmy.pw>
2017-10-08 22:19:22 +0100

    fw99gddqk

--- src/main/resources/shaders/voxel.frag.glsl
+++ src/main/resources/shaders/voxel.frag.glsl
@@ -23,13 +23,11 @@ void main() {
 	vec3 final_colour = vec3(1, 1, 1);
 
 	for (int i = 0; i < u_lights.count; i++) {
-		/* vec3 surface_to_light = normalize(u_lights.lights[i].position - v_surfPos);
+		vec3 surface_to_light = normalize(u_lights.lights[i].position - v_surfPos);
 		float brightness = clamp(dot(surface_to_light, v_normal), 0, 1);
 		float dist = distance(u_lights.lights[i].position, v_surfPos);
 		float attenuation = 1 / (1 + u_lights.lights[i].attenuation_factor * (dist * dist));
-		final_colour *= brightness * attenuation; */
-
-		final_colour = u_lights.lights[i].position;
+		final_colour *= brightness * attenuation;
 	}
 
 	out_colour = vec4(final_colour * v_colour, 1.0);
