7a5f54e9957679eb0a27d2c1297c46783a5fd579
Drew Lemmy <drew@lemmmy.pw>
2017-11-10 14:50:03 +0000

    meow

--- src/main/java/jr/rendering/gdxvox/objects/entities/EntityRenderers.java
+++ src/main/java/jr/rendering/gdxvox/objects/entities/EntityRenderers.java
@@ -2,6 +2,7 @@ package jr.rendering.gdxvox.objects.entities;
 
 import jr.dungeon.entities.EntityAppearance;
 import jr.rendering.gdxvox.objects.entities.renderers.EntityRendererCandlestick;
+import jr.rendering.gdxvox.objects.entities.renderers.EntityRendererCat;
 import jr.rendering.gdxvox.objects.entities.renderers.EntityRendererPlayer;
 import jr.rendering.gdxvox.objects.entities.renderers.EntityRendererTorch;
 
@@ -12,5 +13,6 @@ public class EntityRenderers {
 		m.addRenderers(new EntityRendererTorch(), EntityAppearance.APPEARANCE_TORCH);
 		m.addRenderers(new EntityRendererCandlestick(), EntityAppearance.APPEARANCE_CANDLESTICK);
 		m.addRenderers(new EntityRendererPlayer(), EntityAppearance.APPEARANCE_PLAYER);
+		m.addRenderers(new EntityRendererCat(), EntityAppearance.APPEARANCE_TAMED_CAT);
 	}
 }
