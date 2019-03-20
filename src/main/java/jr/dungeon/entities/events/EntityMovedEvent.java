8c6f5cc577453e78f611a26da80882d903aa7898
Drew Lemmy <drew@lemmmy.pw>
2019-03-20 23:12:00 +0000

    Added deltaX and deltaY to EntityMovedEvent (from vox branch).

--- src/main/java/jr/dungeon/entities/events/EntityMovedEvent.java
+++ src/main/java/jr/dungeon/entities/events/EntityMovedEvent.java
@@ -3,14 +3,22 @@ package jr.dungeon.entities.events;
 import jr.dungeon.entities.Entity;
 import jr.dungeon.events.Event;
 import jr.utils.Point;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class EntityMovedEvent extends Event {
 	private Entity entity;
-	private int lastX, lastY, newX, newY;
+	private int lastX, lastY, newX, newY, deltaX, deltaY;
+	
+	public EntityMovedEvent(Entity entity, int lastX, int lastY, int newX, int newY) {
+		this.entity = entity;
+		this.lastX = lastX;
+		this.lastY = lastY;
+		this.newX = newX;
+		this.newY = newY;
+		this.deltaX = newX - lastX;
+		this.deltaY = newY - lastY;
+	}
 	
 	public Point getLastPosition() {
 		return Point.getPoint(lastX, lastY);
