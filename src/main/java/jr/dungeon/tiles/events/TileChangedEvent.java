360416bb7097c6b6bf81056b42ea3d7ea2018c7e
Drew Lemmy <drew@lemmmy.pw>
2019-03-20 22:55:09 +0000

    Added tile changed event (from vox branch).

---
+++ src/main/java/jr/dungeon/tiles/events/TileChangedEvent.java
@@ -0,0 +1,15 @@
+package jr.dungeon.tiles.events;
+
+import jr.dungeon.events.Event;
+import jr.dungeon.tiles.Tile;
+import jr.dungeon.tiles.TileType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TileChangedEvent extends Event {
+	private Tile tile;
+	private TileType oldType;
+	private TileType newType;
+}
