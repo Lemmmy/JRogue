8fc61975a6eb2cd0845733177b68f9dab4ad3612
Drew Lemmy <drew@lemmmy.pw>
2019-03-25 15:26:39 +0000

    Suppress tile events during level generation, and actually read the eventsSuppressed value. Fixes level generation being extremely slow.

--- src/main/java/jr/dungeon/Level.java
+++ src/main/java/jr/dungeon/Level.java
@@ -97,6 +97,8 @@ public class Level implements Serialisable, Debuggable {
 	protected void generate(Tile sourceTile, Class<? extends DungeonGenerator> generatorClass) {
 		boolean gotLevel = false;
 		
+		tileStore.setEventsSuppressed(true);
+		
 		do {
 			initialise(dungeon);
 			
